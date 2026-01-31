0b9c24b48ec56b4700a1fcb6b14036159296cc63
Tommieboy99 <tomkoek99@hotmail.com>
2026-01-31 13:41:11 +0100

    ejs added as template engine

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = 3000;
