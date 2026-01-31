e70e27023b509ab339246d7183df693baa47af24
Tommieboy99 <tomkoek99@hotmail.com>
2026-01-31 14:36:37 +0100

    route for index page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,24 @@
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    }
+];
+
 const { Router } = require("express");
 
 const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => res.render("index", { messages: messages }))
+indexRouter.post("/new", (req, res) => { 
+    messages.push({ text: req.body.message, user: req.body.name, added: new Date() })
+    res.redirect("/")
+});
 
 module.exports = indexRouter;
