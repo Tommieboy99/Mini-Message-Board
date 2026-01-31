14b18ec5420bdef0a4df271b47a2bde8657e7f01
Tommieboy99 <tomkoek99@hotmail.com>
2026-01-31 14:37:21 +0100

    added express.urlencoded for using formdata

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 
 const path = require("node:path");
+const indexRouter = require('./routes/indexRouter');
+const newRouter = require('./routes/newRouter');
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
